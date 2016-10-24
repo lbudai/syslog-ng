a354347e23e927159d82f08f3c50cf966a0db3f0
Viktor Juhasz <viktor.juhasz@balabit.com>
2016-10-24 15:45:00 +0200

    LOCAL PATCH! |license| add the license_module_init function to the libsyslog-ng-test.a
    This is required for the tests using configuration parse,
    because the configuration parse check the license module.
    With this patch the configuration thinks that loading of the license plugin succedded
    
    Signed-off-by: Viktor Juhasz <viktor.juhasz@balabit.com>

---
+++ libtest/license_module_mock.c
@@ -0,0 +1,7 @@
+#include "license_module_mock.h"
+
+gboolean
+license_module_init(GlobalConfig *cfg, CfgArgs *args)
+{
+  return TRUE;
+}
---
+++ libtest/license_module_mock.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "cfg.h"
+
+gboolean license_module_init(GlobalConfig *cfg, CfgArgs *args);
