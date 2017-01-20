575122fe81bcd64544fa6793c4c80b316da03c68
Laszlo Budai <stentor.bgyk@gmail.com>
2017-01-20 17:21:29 +0100

    stats-query: unit test [WIP/TODO]
    Signed-off-by: Laszlo Budai <stentor.bgyk@gmail.com>

---
+++ tests/unit/test_stats_query.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017 Balabit
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include <criterion/criterion.h>
+
+#include "syslog-ng.h"
+#include "apphook.h"
+#include "stats/stats-cluster.h"
+#include "stats-query-commands.h"
+
+TestSuite(cluster_query_key, .init = app_startup, .fini = app_shutdown);
+
+Test(cluster_query_key, test_global_key)
+{
+  const gchar *expected_key = "dst.file.d_file.instance";
+  StatsCluster *sc = stats_cluster_new(SCS_DESTINATION|SCS_FILE, "d_file", "instance");
+  cr_assert_str_eq(sc->query_key, expected_key,
+               "generated query key(%s) does not match to the expected key(%s)",
+               sc->query_key, expected_key);
+}
+
