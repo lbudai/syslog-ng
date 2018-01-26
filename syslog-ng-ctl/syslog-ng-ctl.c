2cc229f70d7a5c3bfeecffb631c74f33131b90dc
norberttakacs <norbert.takacs@balabit.com>
2018-01-26 14:48:30 +0100

    syslog-ng-ctl: add secret store status query
    Signed-off-by: norberttakacs <norbert.takacs@balabit.com>

--- syslog-ng-ctl/syslog-ng-ctl.c
+++ syslog-ng-ctl/syslog-ng-ctl.c
@@ -76,18 +76,28 @@ _is_response_empty(GString *response)
   return (response == NULL || g_str_equal(response->str, ""));
 }
 
+static void
+clear_and_free(GString *str)
+{
+  if (str)
+    {
+      memset(str->str, 0, str->len);
+      g_string_free(str, TRUE);
+    }
+}
+
 static gint
 _dispatch_command(const gchar *cmd)
 {
   gchar *dispatchable_command = g_strdup_printf("%s\n", cmd);
   GString *rsp = slng_run_command(dispatchable_command);
 
-  if (_is_response_empty(rsp))
-    return 1;
+  if (!_is_response_empty(rsp))
+    printf("%s\n", rsp->str);
 
-  printf("%s\n", rsp->str);
+  clear_and_free(rsp);
 
-  g_string_free(rsp, TRUE);
+  memset(dispatchable_command, 0, strlen(dispatchable_command));
   g_free(dispatchable_command);
 
   return 0;
